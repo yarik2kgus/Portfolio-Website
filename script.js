01c59cfef626c6e4dd41cb870a9fe52f314858b0
Yaroslav <yarik22k@gmail.com>
2024-02-12 13:13:12 +0100

    adding a library

---
+++ script.js
@@ -0,0 +1,13 @@
+VANTA.CELLS({
+    el: ".background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    color1: 0x191942,
+    color2: 0x1c1c77,
+    size: 4.00,
+    speed: 0.60
+  })
